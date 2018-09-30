d52ecfc11e5c78ca54c94d200212c0cae8c60055
DontCry <DontCry@MacPro>
2018-09-30 17:09:32 +0800

    后台众筹商品管理

--- app/Admin/Controllers/ProductsController.php
+++ app/Admin/Controllers/ProductsController.php
@@ -65,7 +65,7 @@ class ProductsController extends Controller
     {
         return Admin::grid(Product::class, function (Grid $grid) {
             // 使用 with 来预加载商品类目数据，减少 SQL 查询
-            $grid->model()->with(['category']);
+            $grid->model()->where('type', Product::TYPE_NORMAL)->with(['category']);
             $grid->id('ID')->sortable();
             $grid->title('商品名称');
             $grid->column('category.name', '类目');
@@ -99,6 +99,7 @@ class ProductsController extends Controller
     {
         // 创建一个表单
         return Admin::form(Product::class, function (Form $form) {
+            $form->hidden('type')->value(Product::TYPE_NORMAL);
             // 创建一个输入框，第一个参数 title 是模型的字段名，第二个参数是该字段描述
             $form->text('title', '商品名称')->rules('required');
             // 添加一个类目字段，使用 Ajax 的方式来搜索添加
