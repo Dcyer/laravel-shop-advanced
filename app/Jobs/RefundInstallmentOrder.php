5225c542af7ac417ce77a46e0f0c0652ffd3f8d5
DontCry <DontCry@MacPro>
2018-10-16 18:09:31 +0800

    计算逾期费

---
+++ app/Jobs/RefundInstallmentOrder.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\Jobs;
+
+use App\Models\Installment;
+use App\Models\Order;
+use Illuminate\Bus\Queueable;
+use Illuminate\Queue\SerializesModels;
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Foundation\Bus\Dispatchable;
+
+class RefundInstallmentOrder implements ShouldQueue
+{
+    use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
+
+    protected $order;
+
+    /**
+     * Create a new job instance.
+     *
+     * @return void
+     */
+    public function __construct(Order $order)
+    {
+        $this->order = $order;
+    }
+
+    /**
+     * Execute the job.
+     *
+     * @return void
+     */
+    public function handle()
+    {
+        // 如果商品订单支付方式不是分期付款、订单未支付、订单退款状态不是退款中，则不执行后面的逻辑
+        if ($this->order->payment_method !== 'installment'
+            || !$this->order->paid_at
+            || $this->order->refund_status !== Order::REFUND_STATUS_PROCESSING) {
+            return;
+        }
+        // 找不到对应的分期付款，原则上不可能出现这种情况，这里的判断只是增加代码健壮性
+        if (!$installment = Installment::query()->where('order_id', $this->order->id)->first())
+    }
+}
